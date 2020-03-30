6a288bede44a8398528be2a4c50925f4623bd2b0
erin taylor <ewtaylor29@gmail.com>
2020-03-30 15:06:14 -0400

    src

--- src/App.js
+++ src/App.js
@@ -3,29 +3,43 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
-
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends:friends
+    friends:friends,
+    clickedOn:[],
+    
   };
-
-  removeFriend = id => {
+  shuffleFriends = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    console.log(friends);
-    // Set this.state.friends equal to the new friends array
+    //const friends = this.state.friends.filter(friend => friend.id !== id);
+    var friends1=this.state.clickedOn
+    const friends = this.state.friends.sort(function() {
+      return 0.5 - Math.random();
+    });
     this.setState({ friends });
+    console.log(friends1)
+    if(friends1.includes(id)){
+      console.log("already clicked")
+      this.alreadyClicked(id)
+    }else{
+    friends1.push(id)
+    this.setState({
+      clickedOn:friends1
+    })
   };
-
+}
+  alreadyClicked = id => {
+    
+  }
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>The Clicky Game</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            removeFriend={this.shuffleFriends}
             id={friend.id}
             key={friend.id}
             name={friend.name}
@@ -38,5 +52,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
